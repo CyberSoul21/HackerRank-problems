033dbd01418eb45d281c5cb6bd1bee4b66187060
wjalmarior <wjalmarior@unal.edu.co>
2022-05-25 23:09:14 -0500

    update

---
+++ week_preparation_Python/day_02/03_counting_sort_1/main.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    n = max(arr)
+    freq = []
+    for i in range(0,n+1):
+        freq.append(arr.count(i))
+    #print(arr)    
+    return freq        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
